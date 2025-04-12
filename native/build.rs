d6eeb8d5b2eceafa7b336cce4964aa977318d142
sebseb7 <your.email@example.com>
2025-04-12 09:22:43 +0200

    Fix build output library name and add debugging for CI

--- native/build.rs
+++ native/build.rs
@@ -2,4 +2,6 @@ extern crate napi_build;
 
 fn main() {
   napi_build::setup();
+  println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,librust_knn_node.so");
+  println!("cargo:warning=Building rust_knn_node with napi-rs");
 } 
